157ddc21136c074796d5f2d1161caa4a7bb833cf
KaigaMunene <kaigamunene@gmail.com>
2021-11-04 10:11:48 +0300

    added score

--- project.py
+++ project.py
@@ -2,12 +2,13 @@
 # while True:
 # print(create_random_number("number"))
 import random
-number = random.choice(range(1,20))
+number = random.choice(range(1,10))
 print("I am thinking of a number between 1 and 10."),
-guess_taken = 0
-while guess_taken < 5:
+guessses_taken = 0
+while guessses_taken < 5:
+    #print(str(number))   
     guess = int(input("Take a guess\n:"))
-    guess_taken = guess_taken + 1
+    guessses_taken = guessses_taken + 1
     if guess < number :
         print("Your geuss is too low")
     if guess > number :
@@ -21,6 +22,21 @@ while guess_taken < 5:
     if guess == number:
         print("correct")
         break
+
+initial_score = 5
+score = initial_score + 1
+# if guess == number:
+#     print(f"Your score is: {score}" )
+# elif guess != number:
+#     print(score - guessses_taken)
+while guess == number:
+    score = score - guessses_taken
+    print(f"Your score is: {score}" )
+    break
+
+
+
+
 
 
  
