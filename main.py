f37e686651844597397dd9cec383135e1634bf38
KaigaMunene <kaigamunene@gmail.com>
2021-11-08 14:42:14 +0300

    created a generate random numbers function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def enter_input():    
   '''
   function for user to insert his/her input.
@@ -8,3 +10,11 @@ def enter_input():
           return int(user_input)
       else:
           print("Enter a number or digit")
+
+def generate_random_numbers():    
+  ''' 
+  function for getting a random number from a range specified by user.
+  '''
+  endpoint = enter_input()
+  random_number = random.randrange(1, endpoint)
+  return random_number
